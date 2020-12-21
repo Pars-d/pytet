02c632e7044231f71a0bad4f76b00adee1e31975
Pars'd /2 <poi@cs.utexas.edu>
2020-12-20 23:37:29 -0600

    can initialize minos

--- pytet.py
+++ pytet.py
@@ -1,26 +1,132 @@
-import pygame
+import pygame as pg
+import random
+from enum import Enum, auto
+
+class Mino(pg.sprite.Sprite):
+    SIZE = 30 # Width of one cell in pixels
+
+    class Shape(Enum):
+        # I, O, T, J, L, S, Z = auto(), auto(), auto(), auto(), auto(), auto(), auto()
+        I = auto()
+        O = auto()
+        T = auto()
+        J = auto()
+        L = auto()
+        S = auto()
+        Z = auto()
+
+    # Coordinates for collision checking and rotation
+    # Origin is what block the rotation axis lies on
+    # Refer to https://vignette.wikia.nocookie.net/tetrisconcept/images/0/07/NESTetris-pieces.png/revision/latest?cb=20061118190922
+    I_coordDict = {0: [(-2,0), (-1,0), (0,0), (1,0)],
+                    1: [(0,-2), (0,-1), (0,0), (0,1)] }
+    O_coordDict = {0: [(0,0), (1,0), (0,1), (1,1)] }  # (0,0) is top left corner
+    J_coordDict = {0: [(-1,0), (0,0), (1,0), (1,1)],
+                    1: [(0,-1), (0,0), (-1,1), (0,1)],
+                    2: [(-1,-1), (-1,0), (0,0), (1,0)],
+                    3: [(0,-1), (1,-1), (0,0), (0,1)] }
+    L_coordDict = {0: [(-1,0), (0,0), (1,0), (-1,1)],
+                    1: [(-1,-1), (0,-1), (0,0), (0,1)],
+                    2: [(1,-1), (-1,0), (0,0), (1,0)],
+                    3: [(0,-1), (0,0), (0,1), (1,1)] }
+    S_coordDict = {0: [(0,0), (1,0), (-1,1), (0,1)],
+                    1: [(0,-1), (0,0), (1,0), (1,1)] }
+    T_coordDict = {0: [(-1,0), (0,0), (1,0), (0,1)],
+                    1: [(0,-1), (-1,0), (0,0), (0,1)],
+                    2: [(0,-1), (-1,0), (0,0), (1,0)],
+                    3: [(0,-1), (0,0), (1,0), (0,1)] }
+    Z_coordDict = {0: [(-1,0), (0,0), (0,1), (1,1)],
+                    1: [(1,-1), (0,0), (1,0), (0,1)] }
+    dictMap = {Shape.I: I_coordDict, Shape.J: J_coordDict,
+                Shape.L: L_coordDict, Shape.S: S_coordDict,
+                Shape.T: T_coordDict, Shape.Z: Z_coordDict,
+                Shape.O: O_coordDict }
+
+    def __init__(self, x, y):
+        super().__init__()
+        
+        # Instance Variables
+        self.shape = random.choice(list(Mino.Shape))
+        self.coordDict = Mino.dictMap[self.shape]
+
+        self.x, self.y, self.rot = x, y, 0
+        self.cells = [pg.Surface((self.SIZE, self.SIZE)) for i in range(4)] # 4 cells that form mino
+
+        for cell in self.cells:
+            cell.fill(pg.Color('palegreen3'))
+            pg.draw.rect(cell, pg.Color('plum4'), (0, 0, self.SIZE, self.SIZE), width=3)
+
+    def move(self):
+        # keys = pg.key.get_pressed()
+        # if keys[pg.K_LEFT]:
+        #     self.x += -1
+        # if keys[pg.K_RIGHT]:
+        #     self.x += 1
+        # if keys[pg.K_DOWN]:
+        #     self.y += 1 
+        events = pg.event.get()
+        for event in events:
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_LEFT:
+                    self.x += -1
+                if event.key == pg.K_RIGHT:
+                    self.x += 1
+                if event.key == pg.K_DOWN:
+                    self.y += 1
+                
+
+    def draw(self, surface):
+        for i in range(4):
+            surface.blit( self.cells[i], 
+              ((self.x + self.coordDict[self.rot][i][0]) * Mino.SIZE, 
+              (self.y + self.coordDict[self.rot][i][1]) * Mino.SIZE) )
+
+
+class Playfield(pg.sprite.Sprite):
+    WIDTH = 10 # in cells
+    HEIGHT = 20
+
+    def __init__(self):
+        # Instance Variables
+        self.surf = pg.Surface((self.WIDTH * Mino.SIZE + 10, self.HEIGHT * Mino.SIZE + 5))
+        self.field = self.surf.subsurface((5, 0, self.WIDTH * Mino.SIZE, self.HEIGHT * Mino.SIZE))
+        
+        self.surf.fill(pg.Color('black'))
+        self.field.fill(pg.Color('grey60'))
+
 
 def main():
-    pygame.init()
+    pg.init()
 
     FPS = 60
-    FramePerSec = pygame.time.Clock()
+    FramePerSec = pg.time.Clock()
 
-    pygame.display.set_caption("minimal program")
+    pg.display.set_caption("game")
      
-    # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((800,600))
-     
-    # define a variable to control the main loop
+    SCREEN = pg.display.set_mode((800,800))
+    SCREEN.fill(pg.Color('grey80'))
+
+    p = Playfield()
+
+    m1 = Mino(5, 0)
+
     running = True
-     
     # main loop
     while running:
-        pygame.display.update()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
         
+        m1.move()
+
+        p.field.fill(pg.Color('grey60'))
+        m1.draw(p.field)
+
+        SCREEN.blit(p.surf, (200, 50))
+
+        # Event Handler 
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+        pg.display.update()
         FramePerSec.tick(FPS)
 
 
