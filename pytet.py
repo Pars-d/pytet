d665d77277bbe2b273d2343c5afa3f34a45d2acc
Pars'd /2 <poi@cs.utexas.edu>
2020-12-22 04:30:28 -0600

    can spawn more minos

--- pytet.py
+++ pytet.py
@@ -2,18 +2,19 @@ import pygame as pg
 import random
 from enum import Enum, auto
 
+# Defined Events
+MOVE_DOWN = pg.USEREVENT + 1
+NEW_MINO = pg.USEREVENT + 2
+e_NEW_MINO = pg.event.Event(NEW_MINO)
+
+# Event Timers
+pg.time.set_timer(MOVE_DOWN, 1000)
+
 class Mino(pg.sprite.Sprite):
-    SIZE = 30 # Width of one cell in pixels
+    SIZE = 30  # Width of one cell in pixels
 
     class Shape(Enum):
-        # I, O, T, J, L, S, Z = auto(), auto(), auto(), auto(), auto(), auto(), auto()
-        I = auto()
-        O = auto()
-        T = auto()
-        J = auto()
-        L = auto()
-        S = auto()
-        Z = auto()
+        I, O, T, J, L, S, Z = auto(), auto(), auto(), auto(), auto(), auto(), auto()
 
     # Coordinates for collision checking and rotation
     # Origin is what block the rotation axis lies on
@@ -50,49 +51,84 @@ class Mino(pg.sprite.Sprite):
         self.coordDict = Mino.dictMap[self.shape]
 
         self.x, self.y, self.rot = x, y, 0
-        self.cells = [pg.Surface((self.SIZE, self.SIZE)) for i in range(4)] # 4 cells that form mino
+        self.cells = [pg.Surface((self.SIZE, self.SIZE)) for i in range(4)]   # 4 cells that form mino
+        self.cellsPos = self.coordDict[self.rot]   # Quicker access to coordinates, Make sure to update on rotation
 
         for cell in self.cells:
             cell.fill(pg.Color('palegreen3'))
             pg.draw.rect(cell, pg.Color('plum4'), (0, 0, self.SIZE, self.SIZE), width=3)
 
-    def move(self):
-        # keys = pg.key.get_pressed()
-        # if keys[pg.K_LEFT]:
-        #     self.x += -1
-        # if keys[pg.K_RIGHT]:
-        #     self.x += 1
-        # if keys[pg.K_DOWN]:
-        #     self.y += 1 
-        events = pg.event.get()
+
+    def move(self, field):
+        def mLeft():
+            for pos in self.cellsPos:
+                if (self.x + pos[0] < 1 
+                  or field.posOccupied[self.x + pos[0] - 1][self.y + pos[1]] is not None):
+                    return
+            self.x += -1
+    
+        def mRight():
+            for pos in self.cellsPos:
+                if (self.x + pos[0] > Playfield.WIDTH - 2 
+                  or field.posOccupied[self.x + pos[0] + 1][self.y + pos[1]] is not None):
+                    return
+            self.x += 1
+
+        def mDown():
+            for pos in self.cellsPos:
+                if (self.y + pos[1] > Playfield.HEIGHT - 2 
+                  or field.posOccupied[self.x + pos[0]][self.y + pos[1] + 1] is not None):
+                    pg.event.post(e_NEW_MINO)
+                    return
+            self.y += 1
+        
+        events = pg.event.get(eventtype=[pg.KEYDOWN, MOVE_DOWN])
         for event in events:
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_LEFT:
-                    self.x += -1
+                    mLeft()
                 if event.key == pg.K_RIGHT:
-                    self.x += 1
+                    mRight()
                 if event.key == pg.K_DOWN:
-                    self.y += 1
+                    mDown()
+            if event.type == MOVE_DOWN:
+                mDown()
                 
 
     def draw(self, surface):
         for i in range(4):
             surface.blit( self.cells[i], 
-              ((self.x + self.coordDict[self.rot][i][0]) * Mino.SIZE, 
-              (self.y + self.coordDict[self.rot][i][1]) * Mino.SIZE) )
+              ((self.x + self.cellsPos[i][0]) * Mino.SIZE, 
+              (self.y + self.cellsPos[i][1]) * Mino.SIZE) )
 
 
 class Playfield(pg.sprite.Sprite):
-    WIDTH = 10 # in cells
+    WIDTH = 10  # in cells
     HEIGHT = 20
 
     def __init__(self):
         # Instance Variables
-        self.surf = pg.Surface((self.WIDTH * Mino.SIZE + 10, self.HEIGHT * Mino.SIZE + 5))
-        self.field = self.surf.subsurface((5, 0, self.WIDTH * Mino.SIZE, self.HEIGHT * Mino.SIZE))
+        self.surf = pg.Surface((Playfield.WIDTH * Mino.SIZE + 10, Playfield.HEIGHT * Mino.SIZE + 5))
+        self.field = self.surf.subsurface((5, 0, Playfield.WIDTH * Mino.SIZE, Playfield.HEIGHT * Mino.SIZE))
+        self.posOccupied = [ [None]*Playfield.HEIGHT for i in range(Playfield.WIDTH) ]  # Stores surfaces for occupied spots
         
         self.surf.fill(pg.Color('black'))
         self.field.fill(pg.Color('grey60'))
+
+    def update_posOccupied(self, mino):
+        for cell, pos in zip(mino.cells, mino.cellsPos):
+            self.posOccupied[mino.x + pos[0]][mino.y + pos[1]] = cell
+
+
+    def draw(self):
+        self.field.fill(pg.Color('grey60'))
+        for x, row in enumerate(self.posOccupied):
+            for y, cell in enumerate(row):
+                if cell is not None:
+                    self.field.blit(cell, (x * Mino.SIZE, y * Mino.SIZE))
+
+
+
 
 
 def main():
@@ -109,20 +145,24 @@ def main():
     p = Playfield()
 
     m1 = Mino(5, 0)
+    
 
     running = True
     # main loop
     while running:
         
-        m1.move()
+        m1.move(p)
 
-        p.field.fill(pg.Color('grey60'))
+        p.draw()
         m1.draw(p.field)
 
         SCREEN.blit(p.surf, (200, 50))
 
         # Event Handler 
         for event in pg.event.get():
+            if event.type == NEW_MINO:
+                p.update_posOccupied(m1)
+                m1 = Mino(5, 0)
             if event.type == pg.QUIT:
                 running = False
 
